07e1e6ceac922c2b8b912ce2a56826a842ae7011
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-09-20 23:16:26 -0300

    first test to ping sweeper API and verify that sweeper format is valid is successful

--- spec/sweeper/sweeper_spec.rb
+++ spec/sweeper/sweeper_spec.rb
@@ -1,4 +1,15 @@
-# retrieves JSON from API and converts to array
-# array is correct and valid
-# create a method that resolves minesweeper
-# send resolved array and original array converted as JSON to Heroku API
+require 'rails_helper'
+require 'open-uri'
+
+RSpec.describe 'Test suite', type: :request do
+  # retrieves JSON from API and converts to array
+  SWEEPER_API = 'https://mine-sweeper-generator.herokuapp.com/solver'.freeze
+
+  it 'retrieves valid JSON from API endpoint' do
+    json = JSON.parse(URI.open(SWEEPER_API).read)
+    expect(json.length).to eql(json[0].length)
+  end
+  # array is correct and valid
+  # create a method that resolves minesweeper
+  # send resolved array and original array converted as JSON to Heroku API
+end
