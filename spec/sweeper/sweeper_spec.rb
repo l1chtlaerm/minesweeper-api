e86b24c23cc963f0f9d0c9a979873e870e51702a
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-09-20 23:34:55 -0300

    test for validity of API endpoint characters

--- spec/sweeper/sweeper_spec.rb
+++ spec/sweeper/sweeper_spec.rb
@@ -2,14 +2,17 @@ require 'rails_helper'
 require 'open-uri'
 
 RSpec.describe 'Test suite', type: :request do
-  # retrieves JSON from API and converts to array
+  # retrieves JSON from API and converts to a valid array
   SWEEPER_API = 'https://mine-sweeper-generator.herokuapp.com/solver'.freeze
+  json = JSON.parse(URI.open(SWEEPER_API).read)
 
-  it 'retrieves valid JSON from API endpoint' do
-    json = JSON.parse(URI.open(SWEEPER_API).read)
+  it 'retrieves valid minesweeper matrix from API endpoint' do
     expect(json.length).to eql(json[0].length)
   end
-  # array is correct and valid
+
+  it 'verifies minesweeper does not have invalid chars' do
+    expect(json.join('')).not_to match(/[^\s\d\+\-|\\*]/)
+  end
   # create a method that resolves minesweeper
   # send resolved array and original array converted as JSON to Heroku API
 end
