f490bced4e5332bb9d93c6be7d841cc4ad9ed15a
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-09-20 23:07:25 -0300

    added pseudocode to sweeper spec for further testing

---
+++ spec/sweeper/sweeper_spec.rb
@@ -0,0 +1,4 @@
+# retrieves JSON from API and converts to array
+# array is correct and valid
+# create a method that resolves minesweeper
+# send resolved array and original array converted as JSON to Heroku API
